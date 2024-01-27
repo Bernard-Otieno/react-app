8869cd851148f4ade1e84a090d57794fd837358a
Bernard-Otieno <bernard.otieno@strathmore.edu>
2024-01-27 20:21:15 +0300

    fix: add color to header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,9 @@ const Header = () => {
       <div className="profile-container">
         <img src="./img/Passport photo.JPG" alt="my picture" id="profile-picture" style={{ height: '100px', width: '100px' }} />
         <h1>Bernard Otieno.</h1>
+       
       </div>
-      <h3>Web developer</h3>
+       <h1 className='Head'>Web developer</h1>
       <nav className="left-aligned">
         <ul>
           <li><a href="#about">About Me</a></li>
